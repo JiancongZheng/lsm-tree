8d87c72128d0c284d414ced9d3f73c4a20304a5f
zhengjc <shambleonion@gmail.com>
2026-03-14 16:23:59 +0800

    FT: implement SST and SSTIterator

---
+++ src/sst/sst.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stdexcept>
+
+#include "../block/block.h"
+#include "../block/block_cache.h"
+#include "../block/block_meta.h"
+#include "../utils/bloom_filter.h"
+#include "../utils/files.h"
+
+namespace LSMT {
+/**
+ * --------------------------------------------------------------------------------------------------------------------------------------------
+ * |           Block Section           |          Meta Section          | Bloom Filter |                   Extra Information                  |
+ * --------------------------------------------------------------------------------------------------------------------------------------------
+ * | Block 1 | Block 2 | ... | Block N | Number | Meta 1 | ... | Meta N |              | Meta Offset | Bloom Offset | Min TRX_ID | MAX TRX_ID |
+ * --------------------------------------------------------------------------------------------------------------------------------------------
+ **/
+class SSTBuilder;
+class SSTIterator;
+
+class SST : public std::enable_shared_from_this<SST> {
+    friend class SSTBuilder;
+
+public:
+    static std::shared_ptr<SST> open(size_t sst_id, FileObj file_obj, std::shared_ptr<BlockCache> block_cache);
+
+    void remove();
+
+    int64_t get_block_id(const std::string &key);
+    
+    std::shared_ptr<Block> get_block(size_t block_id);
+
+    SSTIterator get(const std::string &key, uint64_t trx_id);
+
+    std::string get_fkey() const;
+
+    std::string get_lkey() const;
+
+    size_t get_sst_id() const;
+
+    size_t get_sst_size() const;
+
+    size_t get_block_number() const;
+
+    SSTIterator begin(uint64_t trx_id);
+    
+    SSTIterator end();
+
+    std::optional<std::pair<SSTIterator, SSTIterator>> 
+    iters_monotony_predicate(std::function<int(const std::string &)> predicate);
+
+    std::pair<uint64_t, uint64_t> get_trx_id_range() const;
+
+private:
+    size_t sst_id;
+    FileObj file_obj;
+    std::vector<BlockMeta> meta_entries;
+    uint32_t meta_section_offset;
+    uint32_t bloom_filter_offset;
+    std::string fkey;
+    std::string lkey;
+    std::shared_ptr<BloomFilter> filter;
+    std::shared_ptr<BlockCache> block_cache;
+    uint64_t min_trx_id;
+    uint64_t max_trx_id;
+
+    // uint8_t storage_mode = 0;  // 0=inline 1=Wisckey
+    // std::shared_ptr<VLog> vlog;
+};
+
+} // LOG STRUCTURED MERGE TREE
---
+++ src/sst/sst_iterator.cpp
@@ -0,0 +1,140 @@
+#include "sst.h"
+#include "sst_iterator.h"
+
+namespace LSMT {
+SSTIterator::SSTIterator(std::shared_ptr<SST> sst, uint64_t trx_id) 
+: sst(sst), block_id(0), block_it(nullptr), max_trx_id(trx_id) {
+    if (sst == nullptr || sst->get_block_number() == 0) {
+        return;
+    }
+    block_it = std::make_shared<BlockIterator>(sst->get_block(block_id), 0, trx_id);
+}
+
+SSTIterator::SSTIterator(std::shared_ptr<SST> sst, const std::string &key, uint64_t trx_id)
+: sst(sst), block_id(0), block_it(nullptr), max_trx_id(trx_id) {
+    if (sst == nullptr || sst->get_block_number() == 0) {
+        return;
+    }
+    try {
+        block_id = sst->get_block_id(key);
+        if (block_id == -1 || block_id >= sst->get_block_number()) {
+            block_id = sst->get_block_number();
+            block_it = nullptr;
+            return;
+        }
+        block_it = std::make_shared<BlockIterator>(sst->get_block(block_id), key, trx_id);
+        if (block_it->is_end()) {
+            block_id = sst->get_block_number();
+            block_it = nullptr;
+            return;
+        }
+    } catch (const std::exception &) {
+        block_it = nullptr;
+        return;
+    }
+}
+
+SSTIterator::IteratorItem* SSTIterator::operator->() const {
+    update_current();
+    return &(cached_value.value());
+}
+    
+SSTIterator::IteratorItem SSTIterator::operator*() const {
+    update_current();
+    return cached_value.value();
+}
+
+SSTIterator::BaseIterator &SSTIterator::operator++() {
+    if (block_it == nullptr) {
+        return *this;
+    }
+    ++(*block_it);
+    if (block_it->is_end()) {
+        block_id++;
+        if (block_id < sst->get_block_number()) {
+            block_it = std::make_shared<BlockIterator>(sst->get_block(block_id), 0, max_trx_id);
+        } else {
+            block_it = nullptr;
+        }
+    }
+    return *this;
+}
+
+bool SSTIterator::operator==(const BaseIterator &other) const {
+    if (other.get_iterator_type() != IteratorType::SSTableIterator) {
+        return false;
+    }
+    const SSTIterator &other_sst_iter = dynamic_cast<const SSTIterator&>(other);
+    if (sst != other_sst_iter.sst || block_id != other_sst_iter.block_id) {
+        return false;
+    }
+    if (block_it == nullptr && other_sst_iter.block_it == nullptr) {
+        return true;
+    }
+    if (block_it == nullptr || other_sst_iter.block_it == nullptr) {
+        return false;
+    }
+    return *block_it == *(other_sst_iter.block_it);
+}
+
+bool SSTIterator::operator!=(const BaseIterator &other) const {
+    return !(*this == other);
+}
+
+IteratorType SSTIterator::get_iterator_type() const {
+    return IteratorType::SSTableIterator;
+}
+
+uint64_t SSTIterator::get_trx_id() const {
+    return max_trx_id;
+}
+
+bool SSTIterator::is_end() const {
+    return block_it == nullptr;
+}
+
+bool SSTIterator::is_vld() const {
+    return block_it && !block_it->is_end() && block_id < sst->get_block_number();
+}
+
+void SSTIterator::update_current() const {
+    if (block_it != nullptr && !block_it->is_end()) {
+        cached_value = *(*block_it);
+    } else {
+        cached_value = std::nullopt;
+    }
+}
+
+std::string SSTIterator::get_key() {
+    update_current();
+    if (cached_value.has_value()) {
+        return cached_value->first;
+    } else {
+        throw std::out_of_range("SSTIterator is Invalid");
+    }
+}
+
+std::string SSTIterator::get_val() {
+    update_current();
+    if (cached_value.has_value()) {
+        return cached_value->second;
+    } else {
+        throw std::out_of_range("SSTIterator is Invalid");
+    }
+}
+
+std::pair<HeapIterator, HeapIterator>
+SSTIterator::merge_sst_iterator(std::vector<SSTIterator> iters, uint64_t trx_id) {
+    if (iters.empty()) {
+        return std::make_pair(HeapIterator(), HeapIterator());
+    }
+    HeapIterator heap_beg;
+    HeapIterator heap_end;
+    for (auto iter : iters) {
+        if (iter.is_vld() && !iter.is_end()) {
+            heap_beg.pqueue.emplace(iter.get_key(), iter.get_val(), -iter.sst->get_sst_id(), 0, iter.get_trx_id());
+        }
+    }
+    return std::make_pair(heap_beg, heap_end);
+}
+} // LOG STRUCTURED MERGE TREE
