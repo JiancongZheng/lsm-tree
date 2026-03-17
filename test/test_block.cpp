290824b012b9465a1f91f697de880fa44c1fa23b
zhengjc <shambleonion@gmail.com>
2026-03-17 16:25:32 +0800

    UT: optimize build configuration and add utils tests

--- test/test_block.cpp
+++ test/test_block.cpp
@@ -4,8 +4,9 @@
 #include <memory>
 #include <vector>
 
-#include "../src/block/block.h"
-#include "../src/block/block_iterator.h"
+#include "block/block.h"
+#include "block/block_cache.h"
+#include "block/block_meta.h"
 
 using namespace ::LSMT;
 
@@ -252,6 +253,180 @@ TEST_F(BlockTest, PredicateTest2) {
     };
     EXPECT_EQ(kvpairs, results);
 }
+
+class BlockMetaTest : public ::testing::Test {
+protected:
+    std::vector<BlockMeta> CreateTestMeta() {
+        std::vector<BlockMeta> entries;
+        entries.emplace_back(0, "A000", "A999");
+        entries.emplace_back(100, "B000", "B999");
+        entries.emplace_back(200, "C000", "C999");
+        return entries;
+    }
+};
+
+TEST_F(BlockMetaTest, EncodeAndDecodeTest) {
+    std::vector<BlockMeta> entries = CreateTestMeta();
+    std::vector<BlockMeta> decoded;
+    std::vector<uint8_t> encoded;
+
+    BlockMeta::encode_meta(entries, encoded);
+    BlockMeta::decode_meta(encoded, decoded);
+
+    ASSERT_EQ(entries.size(), decoded.size());
+    for (size_t i = 0; i < entries.size(); ++i) {
+        EXPECT_EQ(entries[i].offset, decoded[i].offset);
+        EXPECT_EQ(entries[i].fkey, decoded[i].fkey);
+        EXPECT_EQ(entries[i].lkey, decoded[i].lkey);
+    }
+}
+
+TEST_F(BlockMetaTest, EmptyMetaTest) {
+    std::vector<BlockMeta> entries;
+    std::vector<BlockMeta> decoded;
+    std::vector<uint8_t> encoded;
+
+    BlockMeta::encode_meta(entries, encoded);
+    EXPECT_FALSE(encoded.empty());
+    BlockMeta::decode_meta(encoded, decoded);
+    EXPECT_TRUE(decoded.empty());
+}
+
+TEST_F(BlockMetaTest, ErrorMetaTest) {
+    std::vector<BlockMeta> entries;
+
+    std::vector<uint8_t> error_data = {1, 2, 3};
+    EXPECT_THROW(BlockMeta::decode_meta(error_data, entries), std::runtime_error);
+
+    std::vector<uint8_t> empty_data;
+    EXPECT_THROW(BlockMeta::decode_meta(empty_data, entries), std::runtime_error);
+
+    std::vector<uint8_t> hash_error;
+    entries = CreateTestMeta();
+    BlockMeta::encode_meta(entries, hash_error);
+    hash_error.back() ^= 1;
+    EXPECT_THROW(BlockMeta::decode_meta(hash_error, entries), std::runtime_error);
+}
+
+TEST_F(BlockMetaTest, SpecialMetaTest) {
+    std::vector<BlockMeta> entries;
+    std::vector<BlockMeta> decoded;
+    std::vector<uint8_t> encoded;
+
+    entries.emplace_back(0,  std::string("fkey\0with\0null", 14),  std::string("lkey\0with\0null", 14));
+    BlockMeta::encode_meta(entries, encoded);
+    BlockMeta::decode_meta(encoded, decoded);
+    ASSERT_EQ(decoded.size(), entries.size());
+    EXPECT_EQ(decoded[0].fkey, std::string("fkey\0with\0null", 14));
+    EXPECT_EQ(decoded[0].lkey, std::string("lkey\0with\0null", 14));
+}
+
+TEST_F(BlockMetaTest, LargeMetaTest) {
+    std::vector<BlockMeta> entries;
+    std::vector<BlockMeta> deocded;
+    std::vector<uint8_t> encoded;
+    
+    for (int i = 0; i < 1000; ++i) {
+        size_t offset = i * 100;
+        char fkey[16], lkey[16];
+        snprintf(fkey, sizeof(fkey), "key%03d00", i);  // key00000 key00100 key00200 ...
+        snprintf(lkey, sizeof(lkey), "key%03d99", i);  // key00099 key00199 key00299 ...
+        entries.emplace_back(offset, fkey, lkey);
+    }
+    BlockMeta::encode_meta(entries, encoded);
+    BlockMeta::decode_meta(encoded, deocded);
+
+    ASSERT_EQ(deocded.size(), entries.size());
+    for (int i = 0; i < 1000; ++i) {
+        EXPECT_EQ(deocded[i].offset, entries[i].offset);
+        EXPECT_EQ(deocded[i].fkey, entries[i].fkey);
+        EXPECT_EQ(deocded[i].lkey, entries[i].lkey);
+    }
+    for (int i = 0; i < 1000; ++i) {
+        EXPECT_LT(deocded[i - 1].lkey, deocded[i].fkey);
+    }
+}
+
+class BlockCacheTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        block_cache = std::make_unique<BlockCache>(3,2);
+    }
+    std::unique_ptr<BlockCache> block_cache;
+};
+
+TEST_F(BlockCacheTest, PutAndGet) {
+    auto block1 = std::make_shared<Block>();
+    auto block2 = std::make_shared<Block>();
+    auto block3 = std::make_shared<Block>();
+
+    block_cache->put(1, 1, block1);
+    block_cache->put(1, 2, block2);
+    block_cache->put(1, 3, block3);
+
+    EXPECT_EQ(block_cache->get(1, 1), block1);
+    EXPECT_EQ(block_cache->get(1, 2), block2);
+    EXPECT_EQ(block_cache->get(1, 3), block3);
+}
+
+TEST_F(BlockCacheTest, CacheEviction1) {
+    auto block1 = std::make_shared<Block>();
+    auto block2 = std::make_shared<Block>();
+    auto block3 = std::make_shared<Block>();
+    auto block4 = std::make_shared<Block>();
+
+    block_cache->put(1, 1, block1);
+    block_cache->put(1, 2, block2);
+    block_cache->put(1, 3, block3);
+
+    block_cache->get(1, 1);
+    block_cache->get(1, 2);
+
+    block_cache->put(1, 4, block4);
+
+    EXPECT_EQ(block_cache->get(1, 1), block1);
+    EXPECT_EQ(block_cache->get(1, 2), block2);
+    EXPECT_EQ(block_cache->get(1, 3), nullptr);
+    EXPECT_EQ(block_cache->get(1, 4), block4);
+}
+
+TEST_F(BlockCacheTest, CacheEviction2) {
+    auto block1 = std::make_shared<Block>();
+    auto block2 = std::make_shared<Block>();
+    auto block3 = std::make_shared<Block>();
+    auto block4 = std::make_shared<Block>();
+
+    block_cache->put(1, 1, block1);
+    block_cache->put(1, 2, block2);
+    block_cache->put(1, 3, block3);
+
+    block_cache->get(1, 1);
+    block_cache->get(1, 2);
+    block_cache->get(1, 3);
+
+    block_cache->put(1, 4, block4);
+
+    EXPECT_EQ(block_cache->get(1, 1), nullptr);
+    EXPECT_EQ(block_cache->get(1, 2), block2);
+    EXPECT_EQ(block_cache->get(1, 3), block3);
+    EXPECT_EQ(block_cache->get(1, 4), block4);
+}
+
+TEST_F(BlockCacheTest, HitRate) {
+    auto block1 = std::make_shared<Block>();
+    auto block2 = std::make_shared<Block>();
+
+    block_cache->put(1, 1, block1);
+    block_cache->put(1, 2, block2);
+
+    block_cache->get(1, 1);
+    block_cache->get(1, 2);
+
+    block_cache->get(1, 3);
+
+    EXPECT_EQ(block_cache->hit_rate(), 2.0 / 3.0);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
---
+++ test/test_utils.cpp
@@ -0,0 +1,141 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <filesystem>
+#include <random>
+
+#include "../src/utils/bloom_filter.h"
+#include "../src/utils/files.h"
+#include "../src/config/config.h"
+
+
+using namespace ::LSMT;
+
+class FileTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        if (!std::filesystem::exists("file_test")) {
+            std::filesystem::create_directory("file_test");
+        }
+    }
+
+    void TearDown() override {
+        // std::filesystem::remove_all("file_test");
+    }
+
+    std::vector<uint8_t> generate_random_data(size_t size)
+    {
+        std::vector<uint8_t> data(size);
+
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = static_cast<uint8_t>(random() % 256);
+        }
+        return data;
+    }
+};
+
+TEST_F(FileTest, BasicReadAndWrite) {
+    const std::string path = "file_test/basic.dat";
+    std::vector<uint8_t> data = {1, 2, 3, 4, 5, 6};
+
+    auto raw_file = FileObj::create_and_write(path, data);
+    EXPECT_EQ(raw_file.size(), data.size());
+
+    auto new_file = FileObj::open(path, false);
+    EXPECT_EQ(new_file.size(), data.size());
+
+    auto read_data = new_file.read(0, data.size());
+    EXPECT_EQ(read_data, data);
+}
+
+TEST_F(FileTest, LargeReadAndWrite) {
+    const std::string path = "file_test/large.dat";
+    const size_t data_size = 1024 * 1024;
+    auto data = generate_random_data(data_size);
+
+    auto raw_file = FileObj::create_and_write(path, data);
+    EXPECT_EQ(raw_file.size(), data_size);
+
+    auto new_file = FileObj::open(path, false);
+    
+    for (size_t offset = 0; offset < data_size; offset += 1024) {
+        auto raw_data = std::vector<uint8_t>(data.begin() + offset, data.begin() + offset + 1024);
+        auto new_data = new_file.read(offset, 1024);
+        EXPECT_EQ(raw_data, new_data);
+    }
+}
+
+TEST_F(FileTest, PartialRead) {
+    const std::string path = "file_test/partial.dat";
+    std::vector<uint8_t> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    auto raw_file = FileObj::create_and_write(path, data);
+    EXPECT_EQ(raw_file.size(), data.size());
+
+    auto new_file = FileObj::open(path, false);
+
+    auto beg_data = new_file.read(0, 2);
+    EXPECT_EQ(beg_data[0], 1);
+    EXPECT_EQ(beg_data[1], 2);
+
+    auto mid_data = new_file.read(2, 3);
+    EXPECT_EQ(mid_data[0], 3);
+    EXPECT_EQ(mid_data[1], 4);
+    EXPECT_EQ(mid_data[2], 5);
+
+    auto end_data = new_file.read(8, 2);
+    EXPECT_EQ(end_data[0], 9);
+    EXPECT_EQ(end_data[1], 10);
+}
+
+// 测试错误情况
+TEST_F(FileTest, ErrorCases) {
+    const std::string path = "file_test/error.dat";
+    std::vector<uint8_t> data = {1, 2, 3};
+
+    auto raw_file = FileObj::create_and_write(path, data);
+    auto new_file = FileObj::open(path, false);
+
+    EXPECT_THROW(new_file.read(2, 2), std::out_of_range);
+    EXPECT_THROW(new_file.read(3, 1), std::out_of_range);
+    EXPECT_THROW(new_file.read(0, 4), std::out_of_range);
+
+    EXPECT_THROW(FileObj::open("nonexistent.dat", false), std::runtime_error);
+}
+
+TEST(BloomFilterTest, BloomFilterOperation) {
+    BloomFilter filter(1000, 0.1);
+
+    for (int i = 0; i < 1000; ++i) {
+        filter.add("bloom_filter" + std::to_string(i));
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        EXPECT_TRUE(filter.possibly_contain("bloom_filter" + std::to_string(i)))
+            << "Key bloom_filter" << i << " should be found in the BloomFilter";
+    }
+
+    int false_positive = 0;
+    for (int i = 1000; i < 2000; ++i) {
+        if (filter.possibly_contain("key" + std::to_string(i))) {
+            false_positive++;
+        }
+    }
+    double false_positive_rate = static_cast<double>(false_positive) / 1000;
+    EXPECT_LE(false_positive_rate, 0.2) << "False positive rate " << false_positive_rate;
+}
+
+TEST(TomlConfigTest, TomleConfigOperation) {
+    TomlConfig config = TomlConfig::get_instance("../config.toml");
+
+    EXPECT_EQ(config.get_lsm_sum_memtable_size(), 1024 * 1024 * 64);
+    EXPECT_EQ(config.get_lsm_per_memtable_size(), 1024 * 1024 * 4);
+    EXPECT_EQ(config.get_lsm_sst_level_ratio(), 4);
+    EXPECT_EQ(config.get_lsm_block_size(), 32768);
+    EXPECT_EQ(config.get_lsm_block_cache_size(), 1024);
+    EXPECT_EQ(config.get_lsm_block_cache_lruk(), 8);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
