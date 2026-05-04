8479454c0bc57a4e7df16320a9e2349990c6aa77
zhengjc <shambleonion@gmail.com>
2026-05-04 14:53:19 +0800

    [FEAT]实现SkipList模板和Iterator模块功能

---
+++ src/skiplist/skiplist_iterator.cpp
@@ -0,0 +1,57 @@
+#include "skiplist.h"
+#include "skiplist_iterator.h"
+
+namespace LSMT {
+SkipListIterator::SkipListIterator() : current(nullptr), rd_lock(nullptr) { }
+
+SkipListIterator::SkipListIterator(std::shared_ptr<SkipListNode> node): current(node) { }
+
+SkipListIterator::IteratorItem SkipListIterator::operator*() const {
+    if (!current) { 
+        throw std::runtime_error("SkipList Iterator Error: dereferencing end iterator");
+    }
+    return {current->key, current->val};
+}
+
+BaseIterator& SkipListIterator::operator++() {
+    if (current) { current = current->next[0]; }
+    return *this;
+}
+
+bool SkipListIterator::operator==(const BaseIterator &other) const {
+    if (other.get_iterator_type() != IteratorType::SkiplistIterator) { 
+        return false; 
+    }
+    auto other_skiplist = dynamic_cast<const SkipListIterator&>(other);
+    return current == other_skiplist.current;
+}
+
+bool SkipListIterator::operator!=(const BaseIterator &other) const {
+    return !(*this == other);
+}
+
+
+IteratorType SkipListIterator::get_iterator_type() const {
+    return IteratorType::SkiplistIterator;
+}
+
+bool SkipListIterator::is_end() const { 
+    return current == nullptr; 
+}
+
+bool SkipListIterator::is_vld() const { 
+    return current != nullptr && !current->key.empty(); 
+}
+
+std::string SkipListIterator::get_key() const { 
+    return current->key; 
+}
+
+std::string SkipListIterator::get_val() const { 
+    return current->val; 
+}
+
+uint64_t SkipListIterator::get_trx_id() const { 
+    return current->trx_id; 
+}
+} // LOG STRUCTURED MERGE TREE
