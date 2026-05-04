1cbe5e104ef93d42b5d4c436bd91c0382612fc68
zhengjc <shambleonion@gmail.com>
2026-05-04 15:01:01 +0800

    [FEAT]实现Memtable模块功能

---
+++ src/memtable/memtable.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <shared_mutex>
+#include <string>
+#include <vector>
+
+#include "memtable_iterator.h"
+#include "skiplist/skiplist.h"
+#include "sst/sst.h"
+
+namespace LSMT {
+class MemTable {
+public:
+    MemTable();
+
+    ~MemTable() = default;
+
+    void put(const std::string &key, const std::string &val, uint64_t trx_id);
+
+    void put(const std::vector<std::pair<std::string, std::string>> &kv_pairs, uint64_t trx_id);
+
+    void remove(const std::string &key, uint64_t trx_id);
+
+    void remove(const std::vector<std::string> &keys, uint64_t trx_id);
+
+    SkipListIterator get(const std::string &key, uint64_t trx_id);
+
+    std::vector<std::pair<std::string, std::optional<std::pair<std::string, uint64_t>>>>
+    get(const std::vector<std::string> &keys, uint64_t trx_id);
+
+    MemtableIterator begin(uint64_t trx_id);
+
+    MemtableIterator end();
+
+    void clear();
+
+    std::shared_ptr<SST> flush(SSTBuilder &builder, std::string &sst_path, size_t sst_index, 
+        std::vector<uint64_t> &trx_ids, std::shared_ptr<BlockCache> block_cache);
+    
+    MemtableIterator iters_preffix(const std::string &preffix, uint64_t trx_id);
+
+    std::optional<std::pair<MemtableIterator, MemtableIterator>> 
+    iters_monotony_predicate(uint64_t trx_id, std::function<int(const std::string&)> predicate);
+
+    size_t get_total_size();
+
+    size_t get_active_size();
+
+    size_t get_frozen_size();
+
+    void freeze_memtable();
+
+private:
+    void freeze_active_table();
+    
+    inline bool get_active(const std::string &key, uint64_t trx_id, SkipListIterator &it);
+
+    inline bool get_frozen(const std::string &key, uint64_t trx_id, SkipListIterator &it);
+
+private:
+    std::shared_ptr<SkipList> active_table;
+    std::vector<std::shared_ptr<SkipList>> frozen_table;
+    size_t frozen_bytes;
+    std::shared_mutex active_mutex;
+    std::shared_mutex frozen_mutex;
+};
+}  // LOG STRUCT MERGE TREE
